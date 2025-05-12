95fb021713ca236e3fc8c2d4b1498e5637ef8474
gbalbuquerque <gui67785@gmail.com>
2025-05-11 22:42:50 -0300

    adição das conexões

--- services/s2/consumer.py
+++ services/s2/consumer.py
@@ -1,17 +1,53 @@
 from kafka import KafkaConsumer
 import json
-
+import time
+from cassandra.cluster import Cluster
+from pymongo import MongoClient
+import psycopg2
 
 consumer = KafkaConsumer(
-    'tempertature_sensor_topic',
+    'Dados_Cod',
     api_version=(3,8,0),
     bootstrap_servers='kafka:9092',
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id= 'tempertature_sensor_consumer_group',
+    group_id= 'cod_group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-if __name__ == '__main__':
-    for message in consumer:
-        print(f"Received: {message.value}")
+def conexao_mongodb():
+    try:
+        client = MongoClient('mongodb://mongo27017/')
+        db = client['cod_db']
+        return db
+    except Exception as e:
+        print(f"Erro de conexão com o mongo {e}") 
+        return None
+
+def conexao_cassandra():
+    try:
+        cluster = Cluster(['cassandra'])
+        conexao = cluster.connect()
+        conexao.execute("""
+            CREATE KEYSPACE IF NOT EXISTS cod_ks
+            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
+        """)
+        conexao.set_keyspace('cod_ks')
+        return conexao
+    except Exception as e:
+        print(f"Erro ao conectar ao Cassandra: {e}")
+        return None
+
+def conexao_postgress():
+    try:
+        conn = psycopg2.connect(
+            host="postgres",
+            database="cod_db",
+            user="postgres",
+            password="postgres"
+        )
+        return conn
+    except Exception as e:
+        print(f"Erro ao conectar ao PostgreSQL: {e}")
+        return None
+    
